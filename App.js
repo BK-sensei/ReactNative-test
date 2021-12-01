dcb19c4b72799af263926ee0fc01a7586be51878
becca <rebecca.kaci@gmail.com>
2021-12-01 15:27:17 +0100

    test jour 1 terminé

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Pressable, Alert, ActivityIndicator, StatusBar } from 'react-native';
 
 export default function App() {
+  const alertPress = () => {
+    Alert.alert(
+      "Respect the rules !",
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+
+      <View style={styles.textContainer}>
+        
+        <Text style={[styles.text, styles.text1]}>Itadakimasu !</Text>
+        <Text style={[styles.text, styles.text2]}>Urusai baka !</Text>
+        <Text style={[styles.text, styles.text3]}>Sumimasen sensei.</Text>
+ 
+        <Image 
+          style={styles.img} 
+          source={{uri: 'https://www.konexio.eu/uploads/1/2/0/2/120245745/konexio-logo_1.png',}} 
+        />
+        <Image 
+          style={styles.img} 
+          source={require('./assets/konexio-logo.png')} 
+        />
+
+        <Pressable onPress={alertPress} style={styles.button}>
+          <Text style={[styles.text2, styles.text3]}>Don't click</Text>
+        </Pressable>
+        
+      </View>
+
+      <ActivityIndicator style={styles.activity} size="large" color="white" />
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'blue',
+    padding: 10,
   },
+  textContainer: {
+    backgroundColor: 'white',
+  },
+  text: {
+    marginTop: 80,
+    marginBottom: 80,
+  },
+  text1: {
+    fontSize: 30,
+  },
+  text2: {
+    textAlign: 'center',
+  },
+  text3: {
+    fontWeight: 'bold',
+  },
+  img: {
+    width: 124,
+    height: 40,
+  },
+  button: {
+    backgroundColor: "yellow",
+    padding: 10,
+    margin: 40,
+  },
+  activity: {
+    margin: 20,
+  }
 });
